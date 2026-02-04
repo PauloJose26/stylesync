94bd182c2ababd655e6512d4a42c0d98bb96dade
Paulo Jose <paulojoliveira.2616@gmail.com>
2026-02-04 09:00:56 -0300

    Criacao das primeiras rotas

---
+++ app/routes/main.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify
+
+
+main_bp = Blueprint("main_bp", __name__)
+
+
+@main_bp.route("/")
+def index():
+    return jsonify({"message": "Bem vindo ao StyleSync"})
+
+
+@main_bp.route("/produtos")
+def get_produtos():
+    return jsonify({"message": "Esta é a rota de listagem dos produtos"})
+
+
+@main_bp.route("/login", methods=["POST"])
+def login():
+    return jsonify({"message": "Esta é a rota para realizar um login"})
