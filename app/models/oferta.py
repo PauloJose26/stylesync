6d5259c373022ca93846a6d5de9494a2611561db
Paulo Jose <paulojoliveira.2616@gmail.com>
2026-02-10 12:21:13 -0300

    Leitura do arquivo e adicionar no banco de dados

--- app/models/oferta.py
+++ app/models/oferta.py
@@ -1,6 +1,4 @@
-from pydantic import BaseModel, Field, ConfigDict
-from typing import Optional
-from bson import ObjectId
+from pydantic import BaseModel, ConfigDict
 from datetime import date
 
 
@@ -11,3 +9,41 @@ class Oferta(BaseModel):
     valor_total: float
 
     model_config = ConfigDict(populate_by_name=True, arbitrary_types_allowed=True)
+
+    def model_dump(
+        self,
+        *,
+        mode="python",
+        include=None,
+        exclude=None,
+        context=None,
+        by_alias=None,
+        exclude_unset=False,
+        exclude_defaults=False,
+        exclude_none=False,
+        exclude_computed_fields=False,
+        round_trip=False,
+        warnings=True,
+        fallback=None,
+        serialize_as_any=False
+    ):
+        data = super().model_dump(
+            mode=mode,
+            include=include,
+            exclude=exclude,
+            context=context,
+            by_alias=by_alias,
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none,
+            exclude_computed_fields=exclude_computed_fields,
+            round_trip=round_trip,
+            warnings=warnings,
+            fallback=fallback,
+            serialize_as_any=serialize_as_any,
+        )
+        
+        if self.data_oferta:
+            data["data_oferta"] = self.data_oferta.strftime("%Y-%m-%d")
+        
+        return data
--- app/routes/main.py
+++ app/routes/main.py
@@ -2,12 +2,12 @@ from flask import Blueprint, jsonify, request, current_app
 import csv
 import os
 import io
+import pickle
 
 from .produtos_blueprint import produtos_bp
 from .usuarios_blueprint import usuarios_bp
 from app.decorators import token_required
 from app.models.oferta import Oferta
-from app.models.produto import *
 from app import db
 
 
@@ -24,25 +24,46 @@ def index():
 @main_bp.route("/sales/upload", methods=["POST"])
 @token_required
 def upload_produto(token):
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return jsonify({"error": "Nenhum arquivo foi enviado"}), 400
-    
-    file = request.files['file']
+
+    file = request.files["file"]
     if file.filename == "":
         return jsonify({"error": "Nenhum arquivo selecionado"}), 400
-    
-    if file and file.filename.endswith('.csv'):
+
+    if file and file.filename.endswith(".csv"):
         csv_stream = io.StringIO(file.stream.read().decode("UTF-8"), newline=None)
         csv_reader = csv.DictReader(csv_stream)
-        
-        sales_to_insert = []
-        error = []
-        
-        for row_num, row in enumerate(csv_reader):
+
+        lista_de_ofertas = []
+        erros_nos_registros = []
+
+        for row_num, row in enumerate(csv_reader, 1):
             try:
-                oferta = Oferta(**row)
-                
-            except ValueError:
-                ...
-    
+                oferta_data = Oferta(**row)
+                lista_de_ofertas.append(oferta_data.model_dump())
+
+            except ValueError as e:
+                erros_nos_registros.append(f"Linha {row_num} com dados inválidos")
+            except Exception:
+                erros_nos_registros.append(
+                    f"Linha {row_num} com erro inesperado nos dados"
+                )
+
+        if lista_de_ofertas:
+            try:
+                print(lista_de_ofertas, end="\n\n")
+                db.ofertas.insert_many(lista_de_ofertas)
+
+            except Exception as e:
+                return jsonify({"erro": e.args}), 400
+
+        return jsonify(
+            {
+                "message": "Upload realizado com sucesso",
+                "vendas_importadas": len(lista_de_ofertas),
+                "erros_encontrados": erros_nos_registros,
+            }
+        )
+
     return jsonify({"message": "Esta é a rota de upload do arquivo de vendas"})
