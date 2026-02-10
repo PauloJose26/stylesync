dcd07acb07668eedfa50987ddbf03f9137165f2b
Paulo Jose <paulojoliveira.2616@gmail.com>
2026-02-10 10:19:03 -0300

    Oferta

--- app/models/oferta.py
+++ app/models/oferta.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional
+from bson import ObjectId
 from datetime import date
 
 
@@ -7,3 +9,5 @@ class Oferta(BaseModel):
     produto_id: str
     quantidade: int
     valor_total: float
+
+    model_config = ConfigDict(populate_by_name=True, arbitrary_types_allowed=True)
--- app/routes/main.py
+++ app/routes/main.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, jsonify, request, current_app
-from pydantic import ValidationError
-from werkzeug.exceptions import UnsupportedMediaType
-from datetime import datetime, timedelta, timezone
-import jwt
+import csv
+import os
+import io
 
-from app.models.usuario_login import LoginPayload
-from app.models.produto import *
 from .produtos_blueprint import produtos_bp
 from .usuarios_blueprint import usuarios_bp
+from app.decorators import token_required
+from app.models.oferta import Oferta
+from app.models.produto import *
 from app import db
 
 
@@ -22,5 +22,27 @@ def index():
 
 
 @main_bp.route("/sales/upload", methods=["POST"])
-def upload_produto():
+@token_required
+def upload_produto(token):
+    if 'file' not in request.files:
+        return jsonify({"error": "Nenhum arquivo foi enviado"}), 400
+    
+    file = request.files['file']
+    if file.filename == "":
+        return jsonify({"error": "Nenhum arquivo selecionado"}), 400
+    
+    if file and file.filename.endswith('.csv'):
+        csv_stream = io.StringIO(file.stream.read().decode("UTF-8"), newline=None)
+        csv_reader = csv.DictReader(csv_stream)
+        
+        sales_to_insert = []
+        error = []
+        
+        for row_num, row in enumerate(csv_reader):
+            try:
+                oferta = Oferta(**row)
+                
+            except ValueError:
+                ...
+    
     return jsonify({"message": "Esta é a rota de upload do arquivo de vendas"})
